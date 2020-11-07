b1617228187100947ecaf80fc0f76a59a37013f8
Александр Зимогоров <zimniimars@gmail.com>
2020-11-08 01:56:38 +0300

    Task 5

--- HW_04/HW_04/HW04.Task2/HW04.Task2/Program.cs
+++ HW_04/HW_04/HW04.Task2/HW04.Task2/Program.cs
@@ -8,26 +8,56 @@ namespace HW04.Task2
         static void Main(string[] args)
         {
             double num1, num2;
+            string action;
             Console.WriteLine("Введите первое число");
             num1 = double.Parse(Console.ReadLine());
             Console.WriteLine("Введите второе число");
             num2 = double.Parse(Console.ReadLine());
-            double num3 = num1 + num2;
+            Console.WriteLine("Выберите операцию + , - ");
+            action = Console.ReadLine();
             Console.WriteLine("Введите сумму чисел");
             double sum1 = double.Parse(Console.ReadLine());
 
-            if (sum1 == num3)
+            switch (action)
             {
-                Console.WriteLine("Правильно");
+                case "+":
+                    double num3 = num1 + num2;
+                    if (sum1 == num3)
+
+                    {
+                        Console.WriteLine("Правильно");
+                    }
+                    else if (sum1 < num3)
+                    {
+                        Console.WriteLine("Неправильно. Ожидалось большее число");
+                    }
+                    else if (sum1 > num3)
+                    {
+                        Console.WriteLine("Неправильно. Ожидалось меньшее число");
+                    }
+                    break;
+                case "-":
+                    double num4 = num1 - num2;
+                   
+                        if (sum1 == num4) 
+                 
+                        {
+                             Console.WriteLine("Правильно");
+                        }
+                        else if (sum1 < num4)
+                        {
+                            Console.WriteLine("Неправильно. Ожидалось большее число");
+                        }
+                        else if (sum1 > num4)
+                        {
+                            Console.WriteLine("Неправильно. Ожидалось меньшее число");
+                        }
+                    break;
+                default:
+                    Console.WriteLine("Неправильно");
+                    break;
             }
-            else if (sum1 < num3)
-            {
-                Console.WriteLine("Неправильно. Ожидалось большее число");
-            }
-            else if (sum1 > num3)
-            {
-                Console.WriteLine("Неправильно. Ожидалось меньшее число");
-            }
+     
 
         }
      
