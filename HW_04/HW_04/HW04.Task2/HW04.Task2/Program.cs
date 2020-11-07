c354774ef6cabebcbde750955c97d71828fb513e
Александр Зимогоров <zimniimars@gmail.com>
2020-11-07 23:35:26 +0300

    task2

---
+++ HW_04/HW_04/HW04.Task2/HW04.Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Dynamic;
+
+namespace HW04.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            Console.WriteLine("Введите первое число");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            num2 = double.Parse(Console.ReadLine());
+            double num3 = num1 + num2;
+            Console.WriteLine("Сумма чисел = " + num3 );
+        }
+    }
+}
