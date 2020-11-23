9275e72fe5be24967a8b1f8f1271d05ddbedabf5
Александр Зимогоров <zimniimars@gmail.com>
2020-11-23 15:44:52 +0300

    Task_1

--- hmw7_t1/hmw7_t1/Program.cs
+++ hmw7_t1/hmw7_t1/Program.cs
@@ -6,7 +6,21 @@ namespace hmw7_t1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] separators = {";"};
+            string numberO = "O";
+            string numberA = "A";
+            string numbero = "o";
+            string numbera = "a";
+            Console.WriteLine("Введите стих, разделяя строки точкой с запятой: ");
+            string value = Console.ReadLine();
+            value = value.Replace(numberO, numberA);
+            value = value.Replace(numbero, numbera);
+            string[] words = value.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Ваш стих:");
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
